331bd40c1efa025d9fd5678b03a9559a9258957b
arnab-zero <darnabhere@gmail.com>
2023-08-09 01:40:48 +0600

    Added 1 source code.

---
+++ 2023/August Arc/Solved/A1841.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define shuru int main(void){
+#define shesh return 0;}
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define vi vector<int>
+#define pii pair<int,int>
+#define pb push_back
+#define mpr make_pair
+#define lp(x,n,d) for(int i=x;i<n;i+=d)
+#define spc " "
+typedef long long int lli;
+
+void solve(){
+
+    int x;
+    cin>>x;
+
+    if(x<=4)
+        cout<<"Bob"<<endl;
+    else 
+        cout<<"Alice"<<endl;
+
+}
+
+
+
+shuru;
+fastio;
+
+int t;
+cin>>t;
+
+while(t--)
+    solve();
+
+shesh;
